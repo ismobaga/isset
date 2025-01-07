cf5faa5cc17abd507380bfb963a2f2ba21572a80
Ismail Bagayoko <8527925+ismobaga@users.noreply.github.com>
2025-01-07 01:32:29 +0000

    working on moodle

--- config/auth.php
+++ config/auth.php
@@ -61,7 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'moodle',
             'model' => App\Models\User::class,
         ],
 
