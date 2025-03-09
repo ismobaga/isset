4a765d5df79f180dd476e361edfb200a454aa314
ismail <ismail@bagayoko.ml>
2025-03-09 18:39:03 -0300

    add French authentication language lines and update validation error messages

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,12 +1,12 @@
 @if ($errors->any())
     <div {{ $attributes }}>
         <div class="px-4 py-2 rounded-lg text-sm bg-red-500 text-white">
-            <div class="font-medium">{{ __('Whoops! Something went wrong.') }}</div>
+            <div class="font-medium">{{ __('Oups ! Quelque chose s\'est mal passé.') }}</div>
             <ul class="mt-1 list-disc list-inside text-sm">
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
-        </div>         
+        </div>
     </div>
 @endif
